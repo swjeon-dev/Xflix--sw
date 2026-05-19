bb07494934615861dbc7b75fa8b465d1f05a841d
jsw <csw9201@naver.com>
2026-05-19 15:08:39 +0900

    refactor(pages): FSD 세그먼트 경로로 페이지 import 정리
    - home·movie-detail 페이지가 shared ui/config와 features model·ui를 사용하도록 연결합니다.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { Helmet } from 'react-helmet-async'
 import { FeaturedMovie } from '@/widget/featured-movie'
-import { HOME_CATEGORIES } from '../features/movies/constants/home-categories'
-import ContentsCarousel from '@/features/movies/components/contents-list'
+import { HOME_CATEGORIES } from '../features/movies/config/home-categories'
+import ContentsCarousel from '@/features/movies/ui/contents-list'
 
 function Home() {
   return (
@@ -16,7 +16,7 @@ function Home() {
             key={category.id}
             title={category.title}
             endPoint={category.endPoint}
-            params={{ region: 'KR' }}
+            params={{ region: 'KR', page: 1 }}
           />
         ))}
       </article>
--- src/pages/movie-detail.tsx
+++ src/pages/movie-detail.tsx
@@ -1,14 +1,11 @@
 import { Helmet } from 'react-helmet-async'
 import { useParams } from 'react-router'
-import Modal from '@/shared/components/ui/Modal'
-import { LoadingComponent } from '@/shared/components/ui/LoadingScreen'
-import { API_ENDPOINT } from '@/features/movies/api/config'
-import ContentsList from '@/features/movies/components/contents-list'
-import useGetMovie from '@/features/movies/hooks/useGetMovie'
-import {
-  FloatingBackButton,
-  MovieDetailSection,
-} from '@/widget/movie-detail'
+import Modal from '@/shared/ui/Modal'
+import { LoadingComponent } from '@/shared/ui/LoadingScreen'
+import { API_ENDPOINT } from '@/shared/config/api-config'
+import ContentsList from '@/features/movies/ui/contents-list'
+import useGetMovie from '@/features/movies/model/get-tmdb-movies'
+import { FloatingBackButton, MovieDetailSection } from '@/widget/movie-detail'
 
 const DETAIL_QUERY = { append_to_response: 'credits' }
 
@@ -28,10 +25,7 @@ function MovieDetail() {
         <title>{movie?.title || 'Detail'}</title>
       </Helmet>
       <article key={id}>
-        <MovieDetailSection
-          movie={movie}
-          error={error}
-        />
+        <MovieDetailSection movie={movie} error={error} />
         <ContentsList
           title='비슷한 장르 영화'
           endPoint={API_ENDPOINT.MOVIE_SIMILAR(id!)}
