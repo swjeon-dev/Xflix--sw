b92e1a918144e74ccfa5df3eba903da7e901fea0
jsw <csw9201@naver.com>
2026-05-27 14:56:43 +0900

    refactor(pages): 에러 페이지/홈 라우트·위젯 정리
    shared `routes.ROOT` 사용으로 에러 페이지 링크를 일관화하고, 홈 페이지는 `HomeUI`로 분리해 pages 책임을 줄였습니다.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 import { FeaturedMovie } from '@/widget/featured-movie'
-import { HOME_MOVIE_CATEGORIES } from '@/features/movies/config/home-movie-categories'
-import { HOME_TV_CATEGORIES } from '@/features/tv/config/home-tv-categories'
-import MovieCarousel from '@/features/movies/ui/contents-list'
-import TVCarousel from '@/features/tv/ui/contents-list'
+import { HomeUI } from '@/widget/home'
 
 function Home() {
   return (
@@ -12,24 +9,7 @@ function Home() {
         <title>Home</title>
       </Helmet>
       <FeaturedMovie />
-      <article>
-        {HOME_MOVIE_CATEGORIES.map((category, idx) => (
-          <MovieCarousel
-            key={idx}
-            title={category.title}
-            endPoint={category.endPoint}
-            params={{ region: 'KR', page: 1 }}
-          />
-        ))}
-        {HOME_TV_CATEGORIES.map((category, idx) => (
-          <TVCarousel
-            key={idx}
-            title={category.title}
-            endPoint={category.endPoint}
-            params={{ region: 'KR', page: 1 }}
-          />
-        ))}
-      </article>
+      <HomeUI />
     </section>
   )
 }
