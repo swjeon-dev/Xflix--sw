5eb3d3d29f3240ace0f690fcfaacee32d89ef40e
jsw <csw9201@naver.com>
2026-05-29 11:10:19 +0900

    refactor(widget): import 경로 이름 수정 (Carousel -> carousel)

--- src/widget/media/ui/index.ts
+++ src/widget/media/ui/index.ts
@@ -1,3 +1,3 @@
 export { default as ContentCard } from './content-card'
 export { default as ContentListUI } from './content-list-ui'
-export { default as ContentsCarousel } from './Carousel'
+export { default as ContentsCarousel } from './carousel'
